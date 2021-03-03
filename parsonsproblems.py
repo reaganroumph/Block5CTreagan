91f911863d7d68eeff9953bca178aa72531295c7
reaganroumph <79872816+reaganroumph@users.noreply.github.com>
2021-03-03 13:07:34 -0700

    Add files via upload

---
+++ parsonsproblems.py
@@ -0,0 +1,32 @@
+"""
+March 2, 2021
+
+Use this document to record your answers to the Parson's Problems
+shared in class.
+
+Remember: the idea behind a Parson's Problem is that you are
+shown completely correct code that is presented OUT OF ORDER.
+When arranged correctly, the code does what the Parson's Problem
+says it should do.
+
+"""
+
+
+#Write your answer to Parson's Problem #1 below:
+for x in range (0,10):
+    print ("hello world")
+
+#Write your answer to Parson's Problem #2 below:
+myList = []
+myList.append("apples")
+myList.append("pineapples")
+print (myList)
+
+#Write your answer to Parson's Problem #3 below:
+x = int(x)
+x = input("type a number")
+if x > 0:
+    print("That're more than nothing!")
+else:
+    print("That's a small number!")
+
