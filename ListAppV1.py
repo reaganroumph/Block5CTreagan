d6164f363a70e8f66482de2a94a02e414cc9b78c
reaganroumph <79872816+reaganroumph@users.noreply.github.com>
2021-03-11 13:25:57 -0700

    Add files via upload

--- ListAppV1.py
+++ ListAppV1.py
@@ -1,14 +1,21 @@
 
 
+
+myList = []
+
 def mainProgram():
-    myList = []
+    while True:
     print("Hello, there! Let's work with lists!")
     print("Choose from the following options, Type a number below!")
-    choice = input("1, add to a list, 2, return the value at an index position! ")
+    choice = input("""1. add to a list or
+2. return the value at an index position!
+3. exit program  """)
     if choice == "1":
         addToList()
     elif choice == "2":
         indexValues()
+    elif choice == "3":
+        break
     #TO ADD: 1. a way to loop the action 2. a way to quit 3. think of repitition 
     
 def addToList():
@@ -18,6 +25,9 @@ def addToList():
     
 
 def indexValues():
+    print("ohhh! I heard you need a particular piece of data!")
+    indexPos = input("What index position are you curious about?")
+    print (myList[int(indexPos)])
 
 if __name__ == "__main__":
     mainProgram()
